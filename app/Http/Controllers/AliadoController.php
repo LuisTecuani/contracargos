2a4d8f35c6ceff3cef986b23fcc50b7d672c06e9
LuisRamos <luisramos@thehiveteam.com>
2019-06-13 14:36:28 -0500

    migracion repsaliado añadida y funcion import corregida

--- app/Http/Controllers/AliadoController.php
+++ app/Http/Controllers/AliadoController.php
@@ -78,8 +78,6 @@ class AliadoController extends Controller
             }else
             {foreach($cards as $ca)
             {
-
-
                 echo $ca->autorizacion.'","';
                 echo $ca->fecha.'","';
                 echo $ca->number.'",';
@@ -98,6 +96,7 @@ class AliadoController extends Controller
 
     public function import(Request $request)
     {
+
         function fix_keys($array) {
             foreach ($array as $k => $val) {
                 if (is_array($val))
@@ -106,46 +105,62 @@ class AliadoController extends Controller
             return array_values($array);
         }
 
-
-$archivos     =   $request->file('files');
+        $archivos     =   $request->file('files');
 
         foreach($archivos as $file)
         {
+            $source = Str::before($file->getClientOriginalName(), '.');
+            $valid = DB::table('consultas.repsaliado as ra')
+                ->where( 'source_file', 'like', $source)->get();
 
-            $rep10 = file_get_contents($file);
-            if (Str::contains($rep10, 'REPORTE DETALLADO DE TRANSACCIONES ACEPTADAS')) {
-                $rep9 = Str::after($rep10, 'REPORTE DETALLADO DE TRANSACCIONES ACEPTADAS                        ');
-                $rep8 = Str::before($rep9, 'Totales:                                                                           ');
-                $rep7 = Arr::sort(preg_split("/\n/", $rep8));
-                $rep6 = preg_grep("/([[:digit:]]{16})/", $rep7);
+            if (count($valid) === 0)
+            {
+                $rep10 = file_get_contents($file);
 
-                foreach ($rep6 as $cls => $vls) {
-                    $rep5[$cls] = preg_grep("/\S/", preg_split("/\s/", $vls));
+                if (Str::contains($rep10, 'REPORTE DETALLADO DE TRANSACCIONES ACEPTADAS'))
+                {
+                    $rep9 = Str::after($rep10, 'REPORTE DETALLADO DE TRANSACCIONES ACEPTADAS                        ');
+                    $rep8 = Str::before($rep9, 'Totales:                                                                           ');
+                    $rep7 = Arr::sort(preg_split("/\n/", $rep8));
+                    $rep6 = preg_grep("/([[:digit:]]{16})/", $rep7);
+                    $rep5 = [];
+
+                    foreach ($rep6 as $cls => $vls)
+                    {
+
+                        $rep5[$cls] = preg_grep("/\S/", preg_split("/\s/", $vls));
+
+                    }
+
+                    $rep4 = fix_keys($rep5);
+
+                    foreach ($rep4 as $rep3)
+                    {
+
+
+                        Repsaliado::create([
+
+                            'tarjeta' => $rep3[0],
+
+                            'user_id' => $rep3[1],
+
+                            'fecha' => $rep3[2],
+
+                            'terminacion' => substr($rep3[0],-4,4),
+
+                            'autorizacion' => $rep3[5],
+
+                            'monto' => $rep3[8],
+
+                            'source_file' => $source
+
+                        ]);
+                    }
                 }
-
-                $rep4 = fix_keys($rep5);
-                foreach ($rep4 as $rep3) {
-
-
-                    Repsaliado::create([
-
-                        'tarjeta' => $rep3[0],
-
-                        'user_id' => $rep3[1],
-
-                        'fecha' => $rep3[2],
-
-                        'autorizacion' => $rep3[5],
-
-                        'monto' => $rep3[8]
-
-                    ]);
-
-                }
-
-
             }
         }
+
 return back();
+
     }
 }
