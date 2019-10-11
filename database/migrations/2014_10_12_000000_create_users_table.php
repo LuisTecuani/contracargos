7ebd291ed37488bd1226eef81f472f39ce4ff1cf
Daniel Carrillo <daniel.carrillo4411@gmail.com>
2019-10-11 12:40:36 -0500

    null value role in users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,8 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->integer('role');
-            $table->timestamp('email_verified_at')->nullable();
+            $table->integer('role')->nullable();
+            $table->timestamp('email_verified_at');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
