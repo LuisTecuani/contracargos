ffa01471405d6c884dceff5186afbc58b6633b1b
Daniel Carrillo <daniel.carrillo4411@gmail.com>
2019-11-13 11:31:25 -0600

    order index sanborns

--- resources/views/sanbornscobro/index.blade.php
+++ resources/views/sanbornscobro/index.blade.php
@@ -2,48 +2,38 @@
 @section('title',"Sanborns")
 @section('content')
 
-            <div class="row">
-                <div class="col-md-2 bg-light mt-2"></div>
-                <div class="col-md-10 mt-10">
-                    <h2><b>Cobros</b></h2>
+    <div class="row">
+        <div class="col-md-1 bg-light mt-1"></div>
+        <div class="col-md-5 mt-5">
+            <h2><b>Cobros</b></h2>
+            <div class="card bg-light mt-4">
+                <div class="card-body">
+                    <form action="{{ route('sanbornsCobroImport') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        @include('contracargos.admin.import_sanborns')
+                    </form>
                 </div>
             </div>
-            <div class="container">
-                <div class="card bg-light mt-3">
-                    <div class="card-header">
-                        Add file to bonificacion_sanborns table
-                    </div>
-                    <div class="card-body">
-                        <form action="{{ route('sanbornsCobroImport') }}" method="POST" enctype="multipart/form-data">
-                            @csrf
-                            @include('contracargos.admin.import_sanborns')
-                        </form>
-                    </div>
+        </div>
+        <div class="col-md-5 mt-5">
+            <h2><b>Devoluciones</b></h2>
+            <div class="card bg-light mt-4">
+                <div class="card-body">
+                    <form action="{{ route('sanbornsDevolucionImport') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        @include('contracargos.admin.import_sanborns')
+                    </form>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-2 bg-light mt-2"></div>
-                <div class="col-md-10 mt-10">
-                    <h2><b>Devos</b></h2>
-                </div>
-            </div>
-            <div class="container">
-                <div class="card bg-light mt-3">
-                    <div class="card-header">
-                        Devos
-                    </div>
-                    <div class="card-body">
-                        <form action="{{ route('sanbornsDevolucionImport') }}" method="POST" enctype="multipart/form-data">
-                            @include('contracargos.admin.import_sanborns')
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <div class="container">
-                <div class="col-md-2 mt-2">
-                </div>
-                <div class="col-md-10 mt-10">
+        </div>
+        <div class="col-md-1 mt-1">
+        </div>
+    </div>
+    <div class="container">
+        <div class="col-md-2 mt-2">
+        </div>
+        <div class="col-md-10 mt-10">
             @include('contracargos.errors_import')
-            </div>
-            </div>
+        </div>
+    </div>
 @endsection
