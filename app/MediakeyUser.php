51f36fdb729f6d645e75d96991cdf7423840dfe7
LuisTecuani <luisramos@thehiveteam.com>
2020-01-09 20:14:59 -0600

    refactor fix

---
+++ app/MediakeyUser.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class MediakeyUser extends Model
+{
+    /**
+     * Specify the connection, since this implements multitenant solution
+     * Called via constructor to faciliate testing
+     *
+     * @param array $attributes
+     */
+    public function __construct($attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->setConnection(config('database.mediakey_connection'));
+    }
+
+    protected $table = 'users';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [];
+
+    /*
+    |--------------------------------------------------------------------------
+    | Eloquent Model Relationships
+    |--------------------------------------------------------------------------
+    */
+
+    /**
+     * Define a one-to-many relationship.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function contracargos() : HasMany
+    {
+        return $this->hasMany(ContracargosMediakey::class, 'user_id')->latest();
+    }
+
+    /**
+     * Define a one-to-many relationship.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function reps() : HasMany
+    {
+        return $this->hasMany(Repsmediakey::class, 'user_id')->latest();
+    }
+}
